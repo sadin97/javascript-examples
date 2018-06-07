5a6418dabec90fe1637db4bc99d84b5b821f71af
sadin97 <32278184+sadin97@users.noreply.github.com>
2018-06-07 12:55:32 +0200

    cleaning code

--- demo.js
+++ demo.js
@@ -1,13 +1,24 @@
+/*var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:1337/');*/
+
+
 /*var godine = 20;
 
 var x = (godine < 18) ? "premlad": "stariji od 18";
 
 document.write(x);*/
 
-function Funkcija(name)
+function PrintHello(name)
 {
-	document.write("Cao, " + name);
+	// console.log("Cao, " + name);
+	document.write("Hello " + name);
 }
+
+PrintHello("Sadin");
 
 /*var person =
 {
